26ba77bb902d0294c145f39706c2cf782fd550ac
scherben <scherben@posteo.de>
2016-10-19 21:29:07 +0200

    First attempts to create the neccesary Objects for the Characters. Currently I am not sure about the ListProperty and the correct setup for inheritance from Entity to Vampire.

---
+++ src/v20toolkit/model/Entity.java
@@ -0,0 +1,38 @@
+package v20toolkit.model;
+
+import javafx.beans.property.*;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by baarzul on 19.10.16.
+ */
+public class Entity {
+    public StringProperty name;
+    public ListProperty<Attribute> attributes;
+    public ListProperty<Ability> abilities;
+
+    /**
+     * Default constructor.
+     */
+    public Entity() { this(null); }
+
+    public Entity(String name) {
+        this.name = new SimpleStringProperty(name);
+        this.abilities = new SimpleListProperty();
+        this.attributes = new SimpleListProperty();
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+}
