1577f1848bc6633432f1fe1500d31a30aa4ee928
scherben <scherben@posteo.de>
2016-10-19 20:33:05 +0200

    implemented main and start methods, rootlayout, initialisation of it and the appropriate rootlayoutcontroller

---
+++ src/v20toolkit/V20toolkit.java
@@ -0,0 +1,58 @@
+package v20toolkit; /**
+ * Created by baarzul on 19.10.16.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import v20toolkit.controller.RootLayoutController;
+
+import java.io.IOException;
+
+public class V20toolkit extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("V20toolkit");
+        this.primaryStage.getIcons().add(new Image("file:resources/images/ident_icon.png"));
+
+        initRootLayout();
+    }
+
+    /**
+     * Initializes the root layout and tries to load the last opened
+     * person file.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(V20toolkit.class.getResource("/v20toolkit/view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+
+            // Give the v20toolkit.controller access to the main app.
+            RootLayoutController controller = loader.getController();
+            controller.setMainApp(this);
+
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
