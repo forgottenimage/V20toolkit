70c5078367d4c41c8641c24d90c0fa8383445469
scherben <scherben@posteo.de>
2016-10-24 17:28:11 +0200

    Experiments with the Replacement of the objectidentifier with the actual name of the personality object.. still not working created a todo for ist

--- src/v20toolkit/view/PersonalityLayoutController.java
+++ src/v20toolkit/view/PersonalityLayoutController.java
@@ -1,7 +1,9 @@
 package v20toolkit.view;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import v20toolkit.V20toolkit;
 import v20toolkit.model.Personality;
@@ -23,6 +25,8 @@ public class PersonalityLayoutController {
 
     @FXML
     private void initialize() {
+        // TODO replace the object identifier in the list with the name ... (should be very easy but I just couldn't find a way..)
+        //personalityList.setCellFactory();
         showPersonalityDetails(null);
         personalityList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showPersonalityDetails(newValue));
     }
